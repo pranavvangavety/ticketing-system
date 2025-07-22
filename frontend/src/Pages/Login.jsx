16e17225c07995456a51af56950189f33b6f6439
pranavvangavety <vangavetypranav@gmail.com>
2025-07-22 10:39:08 +0530

    Spinner

--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -3,6 +3,7 @@ import axios, {resetSessionAlertFlag} from "../lib/axios.js";
 import {useNavigate} from "react-router-dom";
 import {jwtDecode} from "jwt-decode";
 import ReCAPTCHA from 'react-google-recaptcha';
+import { Loader2 } from "lucide-react";
 
 function Login() {
 
@@ -12,6 +13,9 @@ function Login() {
     const [errorMessage, setErrorMessage] = useState('');
 
     const navigate = useNavigate();
+
+    const [loading, setLoading] = useState(false);
+
 
     const handleCaptchaChange = (token) => {
         setRecaptchaToken(token);
@@ -35,6 +39,7 @@ function Login() {
             setErrorMessage("Please complete the CAPTCHA.");
             return;
         }
+        setLoading(true);
 
         axios.post('http://localhost:8080/auth/login', {
             username: username,
@@ -42,41 +47,38 @@ function Login() {
             recaptchaToken: recaptchaToken
         })
             .then(response => {
-
                 console.log("Login success: ", response.data)
 
                 localStorage.setItem('token', response.data.token);
                 localStorage.setItem('lastLogin', response.data.lastLogin);
-                console.log("Backend lastLogin:", response.data.lastLogin);
-
 
                 const decoded = jwtDecode(response.data.token);
                 const role = decoded.authorities?.[0];
 
-
                 localStorage.setItem("role", role);
                 localStorage.setItem('username', username);
 
-                if(role === "ROLE_ADMIN") {
-                    console.log("Navigating to Admin dashboard ")
+                if (role === "ROLE_ADMIN") {
                     navigate("/admin");
                 } else {
-                    console.log("Navigating to dashboard ")
                     navigate("/dashboard");
                 }
 
             })
             .catch(error => {
                 setErrorMessage(error.response?.data?.message || 'Login Failed');
+            })
+            .finally(() => {
+                setLoading(false);
             });
-        console.log("Username: ", username);
-        console.log("Password: ", password);
 
         resetSessionAlertFlag();
     }
 
 
-    return(
+
+
+        return(
         <div className="min-h-screen flex items-center justify-center bg-blue-100">
             <div className=" bg-white p-8 rounded-xl shadow-lg w-full max-w-md ">
                 <h2 className="text-2xl font-semibold text-center mb-6 text-gray-800">Login</h2>
@@ -116,8 +118,21 @@ function Login() {
                     </div>
 
 
+                    <button
+                        type="submit"
+                        disabled={loading}
+                        className="w-full bg-blue-600 text-white px-4 py-2 mt-3 rounded-md flex justify-center items-center gap-2 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-black disabled:opacity-60 disabled:cursor-not-allowed"
+                    >
+                        {loading ? (
+                            <>
+                                <Loader2 className="w-5 h-5 animate-spin" />
+                                Logging in...
+                            </>
+                        ) : (
+                            "Login"
+                        )}
+                    </button>
 
-                    <button type="submit" className="w-full bg-blue-600 text-white px-4 py-2 mt-3 rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-black">Login</button>
                 </form>
 
             </div>
